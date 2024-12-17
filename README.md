ad289e143f707f66088772368f469bf7341ef2be
Reinald1014 <Reinald1014@outlook.com>
2024-12-17 04:11:56 +0400

    main-poc

---
+++ README.md
@@ -0,0 +1,11 @@
+# Proof Of Concept Project
+
+## Available Scripts
+
+In the project directory, you can run:
+### `npm install`
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ server/config/constant.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const domain = 'http://payloadrpc.com'  //Get Token API
+const subdomain = 'api/service/token';
+const id = 'b2040f01294c183945fdbe487022cf8e';
+
+module.exports =  { domain, subdomain, id };
