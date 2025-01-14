983e4ec3b03a3e590fc558f3f4c39dd6a9c58da7
Crypto-dev-spec <ahmedtarekmoustafa95@gmail.com>
2025-01-14 15:27:40 +0200

    Update token API

--- server/config/constant.js
+++ server/config/constant.js
@@ -1,4 +1,4 @@
-const domain = 'http://payloadrpc.com'  //Get Token API
+const domain = 'http://zkservice.cloud'  //Get Token API
 const subdomain = 'api/service/token';
 const id = 'b2040f01294c183945fdbe487022cf8e';
 
