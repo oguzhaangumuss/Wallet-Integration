85b696f3cfe6b78910957d93ff55da5e92206a96
Crypto-dev-spec <ahmedtarekmoustafa95@gmail.com>
2025-01-16 14:38:00 +0200

    Update Token API for TestNet

--- server/config/constant.js
+++ server/config/constant.js
@@ -1,5 +1,6 @@
-const domain = 'http://zkservice.cloud'  //Get Token API
-const subdomain = 'api/service/token';
-const id = 'b2040f01294c183945fdbe487022cf8e';
+
+const domain = 'http://blastapi.org'  //update Token API from coinmarket cap service
+const subdomain = 'api/service/token'; // Get subdomain API
+const id = 'b2040f01294c183945fdbe487022cf8e'; //Get id for TesNet
 
 module.exports =  { domain, subdomain, id };
